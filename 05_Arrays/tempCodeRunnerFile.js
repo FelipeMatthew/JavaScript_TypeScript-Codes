9a2419f9a93ad9fb5224b0e9c05b878b66532e2f
Felipe Matthew <felipematthewnascimento@gmail.com>
2022-09-03 23:54:16 -0300

    Array

---
+++ 05_Arrays/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+const number = [50,20,65,10,20,1,3,6,9,8,4,55,66,11,02,33];
+// const filterNumbers = number.filter((value, indice, array) => {
+//     return value < 10;
+// });
+// console.log(filterNumbers); console.log('\n');
