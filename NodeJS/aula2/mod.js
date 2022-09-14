8f9c04580585ca7c10423157e805248a07ff31b6
Felipe Matthew <felipematthewnascimento@gmail.com>
2022-09-14 16:22:14 -0300

    nodeJS

---
+++ NodeJS/aula2/mod.js
@@ -0,0 +1,9 @@
+// module.exports = (x, y) => x * y;
+// module.exports = class Gatinho{
+//     constructor(nome){
+//         this.nome = nome;
+//     }
+//     miar(){
+//         console.log(`${this.nome} esta miando, miau miau`);
+//     }
+// }
