7ec6c3f5ab1eed3cf738d68747da2d6eb3ed6898
Felipe Matthew <felipematthewnascimento@gmail.com>
2022-09-14 16:22:26 -0300

    nodeJS

---
+++ NodeJS/aula2/app.js
@@ -0,0 +1,9 @@
+// const multiplicacao = require('./mod');
+// console.log(multiplicacao(8, 5));
+
+// const Gatinho = require('./mod');
+// const garfield = new Gatinho('garfield');
+// garfield.miar();
+const path = require('path');
+const pathBonitinho = path.resolve(__dirname, '..', '..', 'arquivos', 'css');
+console.log(pathBonitinho);
