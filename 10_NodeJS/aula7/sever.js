c2eaf8ec34fa55a0ff759e022458ad09a69f77c2
Felipe Matthew <felipematthewnascimento@gmail.com>
2022-09-16 21:44:53 -0300

    servidor

---
+++ 10_NodeJS/aula7/sever.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+// pagina inicial = /
+
+app.get('/', (req, res) => {
+    res.send(`
+        <form action="/" method="POST">
+        Nome: <input type="text" name="nome">
+        <button>Teste ixiiii mano</button>
+        </form>
+    `)
+});
+
+app.post('/', (req, res) => {
+    res.send('Formulário recebido, obigado');
+})
+
+
+app.get('/contato', (req, res) => {
+    res.send(`mopa`);
+})
+
+
+app.listen(3000, () => {
+    console.log('acessar http://localhost:3000');
+    console.log('sever executando na porta 3000');
+    console.log("para finalizar aperte 'ctrl + c'");
+});
